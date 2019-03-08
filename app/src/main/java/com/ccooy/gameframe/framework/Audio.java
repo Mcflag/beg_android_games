f64f0533ea061af01c040cd139a23691b0325755
chen <chenchen@lcsw.cn>
2019-03-08 18:26:35 +0800

    test

---
+++ app/src/main/java/com/ccooy/gameframe/framework/Audio.java
@@ -0,0 +1,7 @@
+package com.ccooy.gameframe.framework;
+
+public interface Audio {
+    public Music newMusic(String filename);
+
+    public Sound newSound(String filename);
+}
---
+++ app/src/main/java/com/ccooy/gameframe/framework/Sound.java
@@ -0,0 +1,7 @@
+package com.ccooy.gameframe.framework;
+
+public interface Sound {
+    public void play(float volume);
+
+    public void dispose();
+}
